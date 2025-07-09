0847b5ba92aa2c2293c9bae628703cf6ab5055e4
Nathaniel Kim <ntckim@gmail.com>
2025-07-09 01:05:25 -0500

    save

---
+++ src/data/technologies.js
@@ -0,0 +1,97 @@
+// Add your technologies data (you might want to move this to a separate file later)
+const technologies = [
+   
+    {
+      name: "Python",
+        icon: "/icons/python.svg",
+      category: "Language"
+    },
+    {
+      name: "HTML",
+        icon: "/icons/html.svg",
+      category: "Language"
+    },
+    {
+      name: "CSS",
+        icon: "/icons/css.svg",
+      category: "Language"
+    },
+    {
+      name: "JS",
+        icon: "/icons/javascript.svg",
+      category: "Language"
+    },
+    {
+        name: "Java",
+      icon: "/icons/java.svg",
+        category: "Language"
+      },
+    {
+        name: "C",
+        icon: "/icons/c.svg",
+        category: "Language"
+      },
+      {
+        name: "SQL",
+        icon: "/icons/sql.svg",
+        category: "Language"
+      },
+      {
+        name: "PHP",
+        icon: "/icons/php.svg",
+        category: "Language"
+      },
+      {
+        name: "mySQL",
+        icon: "/icons/mysql.svg",
+        category: "Tool"
+      },
+      
+      {
+        name: "React",
+        icon: "/icons/react.svg",
+        category: "Tool"
+      },
+      {
+        name: "Astro",
+        icon: "/icons/astro.svg",
+        category: "Tool"
+      },
+      {
+        name: "Firebase",
+        icon: "/icons/firebase.svg",
+        category: "Tool"
+      },
+      {
+        name: "TensorFlow",
+        icon: "/icons/tensorflow.svg",
+        category: "Tool"
+      },
+      {
+        name: "AWS",
+        icon: "/icons/aws.svg",
+        category: "Tool"
+      },
+      {
+        name: "Git",
+        icon: "/icons/git.svg",
+        category: "Tool"
+      },
+      {
+        name: "Figma",
+        icon: "/icons/figma.svg",
+        category: "Tool"
+      },
+      {
+        name: "Photoshop",
+        icon: "/icons/photoshop.svg",
+        category: "Other"
+      },
+      {
+        name: "Premier Pro",
+        icon: "/icons/premiere.svg",
+        category: "Other"
+      },
+  ];
+
+export { technologies }; 
